bd3733be118f3bd4b97c2c8e4b8dbf02f35465b8
leonardo <leo.pohlmann@hotmail.com>
2025-07-30 15:50:02 -0300

    % ligados, idade média, histograma idade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
-from dash import Dash, html, dash_table, dcc, callback, Output, Input, State
+import numpy as np
+import plotly.express as px
+from dash import Dash, html, dcc, callback, Output, Input, State
 
 
 # Declarações
@@ -12,10 +14,111 @@ def padronizacao_diametro(diametro: str):
 
 def preparacao_dados(df: pd.DataFrame):
     df["hidrometro"] = df["Hidrometro"]
-    df.drop(columns=["Hidrometro"])
+
+    df["situacao_ligacao_agua"] = df["Situacao Ligacao Agua"]
 
     df["diametro"] = df["Diametro"].apply(padronizacao_diametro)
-    df.drop(columns=["Diametro"], inplace=True)
+
+    df["data_instalacao"] = df["Data Instalacao"]
+    tempo_instalacao_ate_agora = pd.Timestamp.now() - df["data_instalacao"]
+    df["idade_hidrometro"] = tempo_instalacao_ate_agora.apply(
+        lambda x: round(x.days / 365, 2)
+    )
+
+    df.drop(
+        columns=[
+            "Diametro",
+            "Situacao Ligacao Agua",
+            "Hidrometro",
+            "Idade Hidrometro",
+            "Data Instalacao",
+        ],
+        inplace=True,
+    )
+
+
+def calcular_porcentagem_hidrometros_ligados(df: pd.DataFrame):
+    apenas_ligados = df[df.situacao_ligacao_agua == "LIGADO"]
+
+    contagem_hidrometros = df.hidrometro.count()
+
+    if contagem_hidrometros > 0:
+        porcentagem = (
+            apenas_ligados.situacao_ligacao_agua.count() * 100 / contagem_hidrometros
+        )
+        return porcentagem
+
+    return 0.0
+
+
+def gerar_html_area_dados(df: pd.DataFrame):
+    contagem_hidrometros = df.hidrometro.count()
+
+    porcentagem_hidrometros_ligados = calcular_porcentagem_hidrometros_ligados(df)
+    if porcentagem_hidrometros_ligados == 0.0:
+        porcentagem_hidrometros_ligados = "-"
+    else:
+        porcentagem_hidrometros_ligados = f"{porcentagem_hidrometros_ligados:.2f}"
+
+    idade_media_hidrometros = df.idade_hidrometro.mean()
+    if idade_media_hidrometros is not np.nan:
+        idade_media_hidrometros = f"{idade_media_hidrometros:.2f}"
+    else:
+        idade_media_hidrometros = "-"
+
+    return html.Div(
+        [
+            html.Div(
+                [
+                    html.Div(
+                        contagem_hidrometros,
+                        id="contagem-hidrometros",
+                        **{"data-dado": ""},
+                    ),
+                    html.Div("Nº Total de Hidrômetros"),
+                ],
+                className="quadro-dado",
+            ),
+            html.Div(
+                [
+                    html.Div(
+                        porcentagem_hidrometros_ligados,
+                        id="porcentagem-hidrometros-ligados",
+                        **{"data-dado": ""},
+                    ),
+                    html.Div("Porcentagem de Hidrômetros Ligados"),
+                ],
+                className="quadro-dado",
+            ),
+            html.Div(
+                [
+                    html.Div(
+                        idade_media_hidrometros,
+                        id="idade-media-hidrometros",
+                        **{"data-dado": ""},
+                    ),
+                    html.Div("Idade Média dos Hidrômetros"),
+                ],
+                className="quadro-dado",
+            ),
+            html.Div(
+                [
+                    dcc.Graph(
+                        figure=px.histogram(
+                            df,
+                            x="idade_hidrometro",
+                            nbins=10,
+                            labels={
+                                "idade_hidrometro": "Idade Hidrômetro",
+                            },
+                        )
+                    )
+                ],
+                className="grafico",
+            ),
+        ],
+        id="area-dados",
+    )
 
 
 # Preparação Dados
@@ -27,35 +130,44 @@ preparacao_dados(df)
 # Inicialização App
 app = Dash()
 
-VALORES_DIAMETRO_FILTRO = dict([(int(x), str(x)) for x in df.diametro.unique()])
-VALOR_MINIMO_DIAMETRO = min(VALORES_DIAMETRO_FILTRO.keys())
-VALOR_MAXIMO_DIAMETRO = max(VALORES_DIAMETRO_FILTRO.keys())
+valores_unicos_diametro = [int(x) for x in df.diametro.unique()]
+valores_unicos_diametro.sort()
+VALORES_DIAMETRO_FILTRO = [
+    dict([["label", f"{x}MM"], ["value", int(x)]]) for x in valores_unicos_diametro
+]
+VALOR_MINIMO_DIAMETRO = min(valores_unicos_diametro)
+VALOR_MAXIMO_DIAMETRO = max(valores_unicos_diametro)
 
 app.layout = [
-    dash_table.DataTable(data=df.head().to_dict("records")),
     html.Section(
-        children=[
-            dcc.RangeSlider(
-                min=VALOR_MINIMO_DIAMETRO,
-                max=VALOR_MAXIMO_DIAMETRO,
-                step=5,
-                marks=VALORES_DIAMETRO_FILTRO,
-                value=[VALOR_MINIMO_DIAMETRO, VALOR_MAXIMO_DIAMETRO],
-                included=True,
-                id="filtro-diametro",
-            ),
-            html.Button("Filtrar", id="filtro-submit", type="button"),
-        ],
-    ),
-    html.Section(
-        children=[
+        [
+            html.H2("Filtros"),
             html.Div(
-                children=[
-                    html.Div(children="Nº Total de Hidrômetros:"),
-                    html.Div(children=df.hidrometro.count(), id="contagem-hidrometros"),
+                [
+                    html.Label("Diâmetro Hidrômetro", htmlFor="filtro-diametro"),
+                    dcc.Checklist(
+                        options=VALORES_DIAMETRO_FILTRO,
+                        value=valores_unicos_diametro,
+                        inline=True,
+                        id="filtro-diametro",
+                    ),
+                ]
+            ),
+            html.Div(
+                [
+                    html.Button(
+                        "Filtrar",
+                        id="filtro-submit",
+                        type="button",
+                    ),
                 ]
             ),
         ],
+        id="filtros",
+    ),
+    html.Hr(),
+    html.Section(
+        [gerar_html_area_dados(df)],
         id="secao-resultados",
     ),
 ]
@@ -67,17 +179,10 @@ app.layout = [
     State("filtro-diametro", "value"),
     prevent_initial_call=True,
 )
-def filtro_diametro(n_clicks: int, limites_diametros: list[int]):
-    filtrado = df[
-        (df.diametro >= limites_diametros[0]) & (df.diametro <= limites_diametros[1])
-    ]
+def filtrar(n_clicks: int, limites_diametros: list[int]):
+    filtrado = df[df.diametro.isin(limites_diametros)]
 
-    return html.Div(
-        children=[
-            html.Div(children="Nº Total de Hidrômetros:"),
-            html.Div(children=filtrado.hidrometro.count(), id="contagem-hidrometros"),
-        ]
-    )
+    return gerar_html_area_dados(filtrado)
 
 
 if __name__ == "__main__":
